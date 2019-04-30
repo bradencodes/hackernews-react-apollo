f67cc6dd2222334ae548948e06d5420ca88aca99
Braden Walker <bradencodes@gmail.com>
2019-04-30 13:08:02 -0700

    add routing with React Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import LinkList from './LinkList.js'
 import CreateLink from './CreateLink.js'
+import Header from './Header.js'
+import { Switch, Route } from 'react-router-dom'
 
 class App extends Component {
   render() {
-    return <CreateLink />
+    return (
+      <div className="center w85">
+        <Header />
+        <div className="ph3 pv1 background-gray">
+          <Switch>
+            <Route exact path="/" component={LinkList} />
+            <Route exact path="/create" component={CreateLink} />
+          </Switch>
+        </div>
+      </div>
+    )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './components/App.js';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/index.css'
+import App from './components/App.js'
+import * as serviceWorker from './serviceWorker'
 
-import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { BrowserRouter } from 'react-router-dom'
 
 const httpLink = createHttpLink({
     uri: 'http://localhost:4000'
@@ -19,9 +20,11 @@ const client = new ApolloClient({
 })
 
 ReactDOM.render(
-    <ApolloProvider client={client}>
-        <App />
-    </ApolloProvider>,
+    <BrowserRouter>
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    </BrowserRouter>,
     document.getElementById('root')
 );
 
